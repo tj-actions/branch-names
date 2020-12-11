6f69b01735a7473a96c280acc7060dc9690bff30
Tonye Jack <tonye@coherentpath.com>
2020-12-11 08:33:11 -0500

    switch to use inputs.

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,6 @@
 
 set -e
 
-echo "::set-output name=base_ref_branch::${INPUT_BASE_REF/refs\/heads\//}"
-echo "::set-output name=head_ref_branch::${INPUT_HEAD_REF/refs\/heads\//}"
-echo "::set-output name=ref_branch::${INPUT_REF/refs\/heads\//}"
+echo "::set-output name=base_ref_branch::${0/refs\/heads\//}"
+echo "::set-output name=head_ref_branch::${1/refs\/heads\//}"
+echo "::set-output name=ref_branch::${2/refs\/heads\//}"
