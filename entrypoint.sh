c05e12be63f6d1b669198182e584a5e12ba62de2
Tonye Jack <tonye@coherentpath.com>
2020-12-11 08:41:21 -0500

    Updated to use a composite action

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-echo "::set-output name=base_ref_branch::${0/refs\/heads\//}"
-echo "::set-output name=head_ref_branch::${1/refs\/heads\//}"
-echo "::set-output name=ref_branch::${2/refs\/heads\//}"
